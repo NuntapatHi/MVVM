daf1b8af85e88af6de14011b07f50b5e087b01f4
NuntapatHi <nuntapath@gmail.com>
2024-04-10 13:27:35 +0700

    Initial Commit

---
+++ MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM
+//
+//  Created by Nuntapat Hirunnattee on 10/4/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
